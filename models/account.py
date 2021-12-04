894658eb441923258ae48170d902bc2ef5fbcf52
rocketgithub <rodrigo@aquih.com>
2021-12-03 18:32:18 -0600

    Desplegar el DTE correcto

--- models/account.py
+++ models/account.py
@@ -37,8 +37,8 @@ class AccountMove(models.Model):
                     return
                 
                 dte = factura.dte_documento()
+                logging.warn(dte)
                 xmls = etree.tostring(dte, encoding="UTF-8")
-                logging.warn(xmls)
                 xmls_base64 = base64.b64encode(xmls)
 
                 request_url = "https://ws.ccgfel.gt"
